ecebee0b22bb31918e95447a7cf6453fefc5ef81
Jeffrey <wzhang@wzhangs-MBP.attlocal.net>
2022-07-08 18:32:25 -0700

    added different questions and fixed logic for javascript

--- README.md
+++ README.md
@@ -1,1 +1,93 @@
-# coding-questionaire
+# Password Generator
+
+## Deployment link
+
+https://jeffz98.github.io/coding-questionaire/
+
+## GIF demo
+
+![GIF demo of the application](./Assets/images/demo.gif)
+
+## Screenshots
+![Screenshot of the application's landing page](./Assets/images/PassGenSS.png)
+
+### User Information
+- [LinkedIn](https://www.linkedin.com/in/jeffrey-zhang-52315522a/)
+- [Portfolio](https://jeffz98.github.io/Twilights_Portfolio/)
+
+## Description
+This application generates a password of certian length with the inclusion of special characters, numerics, lowercase characters and uppercase characters.
+
+When the user opens the application, they need to click the generate password button. They will then be prompted for how long they want the password, along with what sort of characters they would like included. The password must be chosen within 8-128 characters and include at least one of the character type options. After selecting the required criteria, the password will be generated.
+
+## Code Snippets
+
+```
+// stores user's choices for character types to be used in password
+function getCharChoices() {
+  var hasSpecChars = false, hasNumChars = false, hasLowerChars = false, hasUpperChars = false;
+  alert("Please choose at least one character type.");
+  hasSpecChars = confirm("Click OK to confirm including special characters.");
+  hasNumChars = confirm("Click OK to confirm including numeric characters.");
+  hasLowerChars = confirm("Click OK to confirm including lowercase characters.");
+  hasUpperChars = confirm("Click OK to confirm including uppercase characters.");
+
+// returns key-value pairs
+  return {
+    specChars: hasSpecChars,
+    numChars: hasNumChars,
+    lowerChars: hasLowerChars,
+    upperChars: hasUpperChars
+  }
+
+  // generates a password based on user selected type, the order is not random here 
+  // and is over the length required in some cases
+  for (var i = 0; i < passLen/charTypeCount; i++) {
+    if (charChoices.specChars) {
+      rand = Math.floor(Math.random() * specialChars.length);
+      tempPwd += specialChars.substring(rand, rand + 1);
+    }
+    if (charChoices.numChars) {
+      rand = Math.floor(Math.random() * nums.length);
+      tempPwd += nums.substring(rand, rand + 1);
+    }
+    if (charChoices.lowerChars) {
+      rand = Math.floor(Math.random() * lowerCharacters.length);
+      tempPwd += lowerCharacters.substring(rand, rand + 1);
+    }
+    if (charChoices.upperChars) {
+      rand = Math.floor(Math.random() * upperCharacters.length);
+      tempPwd += upperCharacters.substring(rand, rand + 1);
+    }
+  }
+
+  // generates a new password
+  var newPwd = shuffle(tempPwd, passLen);
+  return newPwd;
+}
+
+// uses Fisher-Yates shuffling algorithm and returns a password of correct length
+function shuffle(array, len) {
+  var r = array.length, temp, index;
+  // while there remains elemnts to be shuffled
+  while (r) {
+    // randomly pick a remaining element
+    index = Math.floor(Math.random() * r--);
+    // swap with current element
+    temp = array[r];
+    array[r] = array[index];
+    array[index] = temp;
+  }
+  // removes extra letters from password
+  array = array.substring(0, len);
+
+  return array;
+}
+```
+
+In the above code snippet, I'm asking for the user's responses and validating their input within a certain criteria. The first function asks users to select the character type they would like included. The second function creates a temporary password that will have randomly generated characters based off user preference. The third function is the implementation of the Fisher-Yates algorithm that randomizes the password and adjusts the length accordingly.
+
+
+## Technologies Used
+
+HTML, CSS, JavaScript, and Git
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,330 +1,149 @@
+var button = document.querySelector(".button-element")
+var mainContainer = document.getElementById("container")
+var questionsContainer = document.querySelector(".questions-container")
+var timerEl = document.querySelector("#time-left")
+var timer;
+var sec = 60;
+var isCorrect = true
 
-// temporary hold for user score
-var score;
-// The timer
-var countdown = 100;
+var question1 = document.querySelector(".question");
+var option1 = document.querySelector("#option1");
+var option2 = document.querySelector("#option2");
+var option3 = document.querySelector("#option3");
+var option4 = document.querySelector("#option4");
+var userAnswer = document.getElementById("user-answer")
+var scoreContainer = document.getElementById("score-container")
 
-// Pointers to different parts of the site
-var quizContainer = document.getElementById("main-container");
-var time = document.getElementById("time");
-var highScoresContainer = document.getElementById("highscore");
+var currentQuestion = 0
+var numberCorrectQuestions = 0 
 
-// The quiz array stores in each index: the question, an array of each choice, the index of the right answer
-var questionnaire = [
-    { 
+//object that contains the 5 questions, answer choices, and correct answer
+var myQuiz = [
+    {
         question: "Inside which HTML element do we put the JavaScript?",
-        answers: ["<scripting>", "<javscript>", "<js>", "<script>"],
-        correctAnswer: 4
+        answers: [
+            {a: "<scripting>", isCorrect: false}, 
+            {b: "<javscript>", isCorrect: false}, 
+            {c: "<js>", isCorrect: false}, 
+            {d: "<script>", isCorrect: true}
+        ]
     },
-    { 
+    {
         question: "What is the correct JavaScript syntax to write 'Hello World'?",
-        answers: ["response.write('Hello World')", "'Hello World'", "document.write('Hello World')", "('Hello World')"],
-        correctAnswer: 3
-    },
-    { 
-        question: "Where is the correct place to insert a JavaScript?",
-        answers: ["Both the <head> section and the <body> section are correct", "The <body> section", "The <head> section"],
-        correctAnswer: 0
-    },
-    { 
-        question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
-        answers: ["<script src='xxx.js'>", "<script name='xxx.js'>", "<script href='xxx.js'>", "<script value='xxx.js'>"],
-        correctAnswer: 0
-    },
-    {
-        question: "An external JavaScript must contain the <script> tag",
-        answers: ["False", "True"],
-        correctAnswer: 0
-    },
-    {
-        question: "The Web security issues are involved in",
-        answers: ["Server", "CGI script", "Client", "All of the above"],
-        correctAnswer: 3
+        answers: [
+            {a: "response.write('Hello World')", isCorrect: false}, 
+            {b: "'Hello World'>", isCorrect: false}, 
+            {c: "document.write('Hello World')", isCorrect: false}, 
+            {d: "('Hello World')", isCorrect: true}
+        ]
     },
     {
         question: "The majority of a typical Web document will be found in:",
-        answers: ["the head tag", "the title tag", "the body tag", "a comment tag"],
-        correctAnswer: 3
+        answers: [
+            {a: "the head tag", isCorrect: false}, 
+            {b: "the title tag", isCorrect: false}, 
+            {c: "the body tag", isCorrect: false}, 
+            {d: "a comment tag", isCorrect: true}
+        ]
     },
-    
-]
-
-// Helper function: Save current user object to local storage
-function updateLocalStorage(userArray) {
-    localStorage.setItem("scores", JSON.stringify(userArray));
-}
-
-// Render landing page
-function landingPage() {
-    // Ensure quiz container is empty before rendering
-    quizContainer.innerHTML = "";
-
-    // Change the id of quizContainer to state the styling for this screen
-    quizContainer.setAttribute("id", "main-container");
-
-    // Landing page container
-    var div = document.createElement("div");
-    div.setAttribute("id", "landing-page");
-
-    // Heading 1
-    var header1 = document.createElement("h1");
-    header1.textContent = "Coding Assessment";
-    div.appendChild(header1);
-
-    // Heading 2
-    var header2 = document.createElement("h2");
-    header2.textContent = "A Practice Guide";
-    div.appendChild(header2);
-
-    // Heading 3
-    var header3 = document.createElement("h3");
-    header3.textContent = "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!";
-    div.appendChild(header3);
-
-    // Button to start quiz
-    var quizBtn = document.createElement("button");
-    quizBtn.setAttribute("id", "start-btn");
-    quizBtn.textContent = "Start Quiz";
-    // When the user clicks the "Start" button in the landing page, the quiz and the timer start
-    quizBtn.addEventListener("click", startQuiz);
-    div.appendChild(quizBtn);
-
-    // Append landing page container to the site's main container
-    quizContainer.append(div);
-
-    // Populate array with local storage info if the array was empty
-    // Check if localStorage and userArray is empty or not to determine what to render
-    if (localStorage.getItem("scores") !== null && userArray.length === 0) {
-        var localData = JSON.parse(localStorage.getItem("scores"));
-        localData.forEach(user => {
-            userArray.push({ userScore: user["userScore"], userInitials: user["userInitials"] });
-        })
+    {
+        question: "Where is the correct place to insert a JavaScript?",
+        answers: [
+            {a: "Both the <head> section and the <body> section are correct", isCorrect: true}, 
+            {b: "The <body> section", isCorrect: false}, 
+            {c: "The <head> section", isCorrect: false},
+            {d: "None of the above", isCorrect: false} 
+        ]
+    },
+    {
+        question: "The Web security issues are involved in",
+        answers: [
+            {a: "Server", isCorrect: false}, 
+            {b: "CGI script", isCorrect: false}, 
+            {c: "Client", isCorrect: false}, 
+            {d: "All of the above", isCorrect: true}
+        ]
     }
-}
-
-// Helper function to retrieve data from local storage
-function getScoresFromLocalStorage(quizContainer) {
-    // Check if localStorage is empty or not to determine what to render
-    if (localStorage.getItem("scores") !== null) {
-        // Get user info from localStorage
-        var arr = JSON.parse(localStorage.getItem("scores"));
-
-        // Order from highest to lowest score by first saving each userScore in an array, sort the array in descending order, and get corresponding userInitials
-        var scoreArr = arr.map(userObj => userObj.userScore);
-        scoreArr.sort((a, b) => b - a);
-        
-        // Append localStorage results to the screen in descending order
-        for (var i = 0; i < scoreArr.length; i++) {
-            var j = 0;
-            while (j < arr.length) {
-                if (scoreArr[i] === arr[j].userScore) {
-                    var result = document.createElement("p");
-                    result.setAttribute("class", "score-text");
-                    result.textContent = (i + 1) + ". " + arr[j].userInitials + "   -   " + arr[j].userScore;
-                    quizContainer.appendChild(result);
-                }
-                j++;
-            }
-        } 
-    } else {
-        // If localStorage is empty, announce user they haven't played yet
-        var result = document.createElement("p");
-        result.setAttribute("class", "score-text");
-        result.textContent = "No scores have been registered yet.";
-        quizContainer.appendChild(result);
-    }
-}
-
-// Render high scores page
-function renderHighScores() {
-    // Ensure quiz container is empty before rendering
-    quizContainer.innerHTML = "";
-    // Show link to view high scores board after user is done taking the quiz
-    highScoresContainer.style.visibility = "visible";
-    // Reset countdown
-    countdown = 75;
-
-    // Change the id of quizContainer to state the styling for this screen
-    quizContainer.setAttribute("id", "highscore-pg");
-
-    var title = document.createElement("h1");
-    title.textContent = "High Scores";
-    quizContainer.appendChild(title);
-
-    // Get user information from local storage
-    getScoresFromLocalStorage(quizContainer);
-
-    // Container for the next three buttons
-    var div = document.createElement("div");
-    div.setAttribute("class", "btn-container");
-    quizContainer.appendChild(div);
-
-    // Button that allows user to retake the quiz
-    var backBtn = document.createElement("button");
-    backBtn.setAttribute("class", "highscore-btn");
-    backBtn.textContent = "Take quiz";
-    div.appendChild(backBtn);
-
-    backBtn.addEventListener("click", () => {
-        // Reset countdown
-        countdown = 75;
-        startQuiz();
-    });
-
-    // Button that allows user to clear scores board
-    var clearScoresBtn = document.createElement("button");
-    clearScoresBtn.setAttribute("class", "highscore-btn");
-    clearScoresBtn.textContent = "Clear high scores";
-    div.appendChild(clearScoresBtn);
-
-    clearScoresBtn.addEventListener("click" , () => {
-        localStorage.clear();
-        userArray = [];
-        renderHighScores();
-    })
- 
-    // Button that takes user to landing page
-    var mainBtn = document.createElement("button");
-    mainBtn.setAttribute("class", "highscore-btn");
-    mainBtn.textContent = "Go back to landing page";
-    div.appendChild(mainBtn);
-
-    // Taking user back to the landing page
-    mainBtn.addEventListener("click", landingPage);
-}
-
-// When the highScoresContainer link is clicked, take them to the high scores board
-highScoresContainer.addEventListener("click", renderHighScores);
+];
 
 
-
-
-// Function to update user's score and timer
-function checkAnswer(questionObj, answer) {
-    if (questionObj.correctAnswer === questionObj.answers.indexOf(answer)) {
-        score++;
-    } else {
-        countdown -= 10;
-    }
-}
-
-// Function that displays each question
-// The 'i' variable determines the question displayed
-var i = 0;
-// The 'endQuiz' variable indicates all questions have been answered and the timer must stop
-var endQuiz = false;
-function displayQuiz(i) {
-    // Exit and stop timer if we are past the last question
-    if (i === 7) {
-        endQuiz = true;
-        return;
-    }
-    // Empty main container
-    quizContainer.innerHTML = "";
-    // Change the id of quizContainer to state the styling for this screen
-    quizContainer.setAttribute("id", "quiz-screen");
-    // Hide link to view high scores board when user is taking the quiz
-    highScoresContainer.style.visibility = "hidden";
-
-    // Displays question
-    var quizQuestion = document.createElement("h1");
-    quizQuestion.setAttribute("class", "title");
-    quizQuestion.textContent = questionnaire[i].question;
-    quizContainer.appendChild(quizQuestion);
-
-    // Container used to place answer choices vertically
-    var div = document.createElement("div");
-    div.setAttribute("class", "choice-container")
-    quizContainer.appendChild(div);
-
-    // Displays answer choices
-    questionnaire[i].answers.forEach(answer => {
-        var answerBtn = document.createElement("button");
-        answerBtn.setAttribute("class", "choice-btn")
-        answerBtn.textContent = answer;
-        div.appendChild(answerBtn);
-
-        // When an answer is clicked, the question changes
-        answerBtn.addEventListener("click", () => {
-            // Update user's score and timer depending on answer selection
-            checkAnswer(questionnaire[i], answer);
-            i++;
-            displayQuiz(i);
-        })
-    })
-}
-
-// Function that runs when the user clicks the "Start" button in the landing page
-function startQuiz() {
-    displayQuiz(i);
-
-    // Set score to 0 every time the quiz is taken
-    score = 0;
-
-    // In here, the timer begins
-    var quizTimer = setInterval(function () {
-        countdown--;
-        time.innerHTML = "Time: " + countdown;
-
-        // If the user finishes quiz or runs out of time, take them to the post quiz screen
-        if (endQuiz || countdown <= 0) {
-            clearInterval(quizTimer);
-            postQuiz();
+//this function creates the timer that counts down from 60 and stops at 0
+function timerStart () {
+    timer = setInterval(function name() {
+        timerEl.innerText = sec;
+        sec --;
+    if (sec < 0) {
+        score()
         }
-    }, 1000)
-}
-
-
-// Once the user ends the quiz, display their score and ask for their initials for the scoreboard
-function postQuiz() {
-    quizContainer.innerHTML = "";
-    time.innerHTML = "Time: 0";
-
-    // Change id of quizContainer to state the styling for this screen
-    quizContainer.setAttribute("id", "post-quiz");
+    }, 1000);
     
-    // Container to style label and input for initials
-    var div = document.createElement("div");
-    div.setAttribute("class", "initials-container");
-
-    var finalMessage = document.createElement("h1");
-    finalMessage.textContent = "Nice work!";
-
-    var scoreMessage = document.createElement("h2");
-    scoreMessage.textContent = "Your final score is " + score + " out of 7.";
-
-    var nameLabel = document.createElement("label");
-    nameLabel.textContent = "Your initials: ";
-    nameLabel.setAttribute("for", "initials")
-
-    var nameInput = document.createElement("input");
-    nameInput.setAttribute("type", "text");
-    nameInput.setAttribute("id", "initials");
-
-    var submitBtn = document.createElement("button");
-    submitBtn.setAttribute("id", "input-submit");
-    submitBtn.textContent = "Submit";
-
-    // Append everything to the page
-    quizContainer.appendChild(finalMessage);
-    quizContainer.appendChild(scoreMessage);
-    quizContainer.appendChild(div);
-    div.appendChild(nameLabel);
-    div.appendChild(nameInput);
-    quizContainer.appendChild(submitBtn);
-
-    // When button is pressed, save user's info and take them to the high scores board
-    submitBtn.addEventListener("click", () => {
-        if (nameInput.value == "") {
-            alert("Please write your initials");
-        } else {
-            userArray.push({ userScore: score, userInitials: nameInput.value });
-            updateLocalStorage(userArray);
-            renderHighScores();
-        }
-    })
 }
-// Clear userArray and load landing page
-// to store the current user's initials and score
-var userArray = [];
-landingPage();
+
+//when the start button is clicked, the paragraph and button disappear, the timer starts, and the first question appears
+button.addEventListener("click", function startButton() {
+   mainContainer.setAttribute("style", "display: none");
+   questionsContainer.setAttribute("style", "display: flex");
+    timerStart();
+    questionInput();
+})
+
+option1.addEventListener("click", next)
+option2.addEventListener("click", next)
+option3.addEventListener("click", next)
+option4.addEventListener("click", next)
+
+
+//this function moves through the questions, if the answer is incorrect it will display a message that is incorrect, and if the answer is correct, it will display a message that is correct
+function next() {
+    var answer = this.getAttribute("id") 
+    if (answer === "option1" && myQuiz[currentQuestion].answers[0].isCorrect === false) {
+        userAnswer.textContent = "That answer is incorrect.";
+        sec-=10
+    }else if (answer === "option2" && myQuiz[currentQuestion].answers[1].isCorrect === false) {
+        userAnswer.textContent = "That answer is incorrect.";
+        sec-=10
+    } else if (answer === "option3" && myQuiz[currentQuestion].answers[2].isCorrect === false) {
+        userAnswer.textContent = "That answer is incorrect.";
+        sec-=10
+    } else if (answer === "option4" && myQuiz[currentQuestion].answers[3].isCorrect === false) {
+        userAnswer.textContent = "That answer is incorrect.";
+        sec-=10
+    } else {
+        numberCorrectQuestions++;
+        userAnswer.textContent = "That answer is correct.";
+    }
+    if(currentQuestion < myQuiz.length-1) {
+        currentQuestion++;
+        questionInput();
+    } else {
+        score()
+    }
+}
+
+//this function displays the text content displayed in the myQuiz array depending on the current question
+function questionInput() {
+    question1.textContent = myQuiz[currentQuestion].question;
+    option1.textContent = myQuiz[currentQuestion].answers[0].a;
+    option2.textContent = myQuiz[currentQuestion].answers[1].b;
+    option3.textContent = myQuiz[currentQuestion].answers[2].c;
+    option4.textContent = myQuiz[currentQuestion].answers[3].d;
+}
+
+//this function clears the time so it stops at 0 instead of going into the negatives; it also makes the questions container disapper and displays the score
+function score() {
+    clearInterval(timer);
+    timerEl.innerText = sec;
+    questionsContainer.setAttribute("style", "display: none");
+    scoreContainer.setAttribute("style", "display: flex");
+    document.getElementById("score-number").textContent = "Score: " + (numberCorrectQuestions + sec);
+}
+
+var submit = document.getElementById("submit-button")
+
+// this is listening for the submit button to be clicked; when clicked, it shows the user's previous scores
+submit.addEventListener("click", function() {
+    var userName = document.getElementById("user-name").value;
+    var previousScore = JSON.parse(localStorage.getItem("Javascript-Quiz")) || []
+    previousScore.push({user: userName, score: (numberCorrectQuestions + sec)}) 
+    localStorage.setItem("Javascript-Quiz", JSON.stringify(previousScore));
+})
